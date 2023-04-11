75b76cca41287d82d54f6f3d1421329791445bea
Davi Faria <davifariagoncalves@gmail.com>
2023-04-11 11:37:42 -0300

    Segundo commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -12,3 +12,29 @@ var progresso = 100;
 
 var barraDeProgresso = document.querySelector(".progresso_3");
 barraDeProgresso.style.width = progresso + "%";
+
+// progresso estudos
+
+var progresso = 1;
+
+var barraDeProgresso = document.querySelector(".progresso_4");
+barraDeProgresso.style.width = progresso + "%";
+
+var progresso = 1;
+
+var barraDeProgresso = document.querySelector(".progresso_5");
+barraDeProgresso.style.width = progresso + "%";
+
+var progresso = 10;
+
+var barraDeProgresso = document.querySelector(".progresso_6");
+barraDeProgresso.style.width = progresso + "%";
+
+//converte html
+
+function Myelement() {
+    var Newelement02 = document.getElementById("element02");
+    Newelement02.style.display = "block";
+    var Newelement01 = document.getElementById ("element01")
+    Newelement01.style.display = "none" ;
+  }
