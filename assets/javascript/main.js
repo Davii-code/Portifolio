bc641135cb6d67b18b4cba15c464f214eea47130
Davii-Code <davifarigoncalves@gmail.com>
2023-04-10 23:29:32 -0300

    primeiro commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,14 @@
+var progresso = 100;
+
+var barraDeProgresso = document.querySelector(".progresso_1");
+barraDeProgresso.style.width = progresso + "%";
+
+var progresso = 100;
+
+var barraDeProgresso = document.querySelector(".progresso_2");
+barraDeProgresso.style.width = progresso + "%";
+
+var progresso = 100;
+
+var barraDeProgresso = document.querySelector(".progresso_3");
+barraDeProgresso.style.width = progresso + "%";
